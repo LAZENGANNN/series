54cbdc3b5f7252dfa843e6476391b5a3b11785c0
LAZENGANNN <113790875+LAZENGANNN@users.noreply.github.com>
2022-10-22 19:32:24 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,101 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "rus");
+	/*int i = 20;
+	while (i <= 30) {
+		if (i % 2 != 2){
+			cout << i << " ";
+	}
+	i += 1;
+	}*/
+
+	//series 1
+	/*int i = 1;
+	int sum = 0;
+	int a;
+	while (i <= 10) {
+		cin >> a;
+        sum = sum + a;
+		i++;
+	 }
+	cout << sum;*/
+
+	//series 2
+	/*double sum = 1;
+	double a;
+	for (int i = 1; i <= 10; i++) {
+		cin >> a;
+		sum = sum * a;
+
+	}
+	cout << sum;*/
+
+
+	//series 3
+	/*int i = 1;
+	idouble sum = 0;
+	int a;
+	while (i <= 10) {
+		cin >> a;
+		sum = sum + a;
+		i++;
+	}
+	cout << (sum / 10);*/
+
+	//series 4
+	/*int n;
+	int x;
+	cin >> n;
+	double sum = 0;
+	int pr = 1;
+
+	for (int i = 1; i <= n; i++) {
+		cin >> x;
+		sum = x + sum;
+		pr = pr * x;
+	}
+	cout << sum << endl << pr;*/
+
+
+	//series 5
+	/*int n;
+	int i = 1;
+	cin >> n;
+	double sum = 0;
+	double x;
+	while (i <= n) {
+		cin >> x;
+		cout <<int(x) << endl;
+		sum = sum + int(x);
+		i++;
+
+	}
+	cout << "summa =" << sum;*/
+
+	//series 6
+	/*int n;
+	int i = 1;
+	cin >> n;
+	double sum = 1;
+	double x;
+	while (i <= n) {
+		cin >> x;
+		cout << x - int(x) << endl;
+		sum = sum * (x - int(x));
+		i++;
+
+	}
+	cout << "sum =" << sum;*/
+
+
+
+
+		
+
+
+
+
+
+}
